25f476e497a0f8f5c9b6b815ce36529e879bca92
ed_bolma <ed.bolma21@gmail.com>
2022-07-26 15:45:00 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,8 @@
+import getMovies from 'service/moviesAPI';
+import { useEffect } from 'react';
+
 export const App = () => {
+  useEffect(() => {}, []);
+
   return <div>React homework template</div>;
 };
---
+++ src/service/moviesAPI.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const KEY = 'e50a0796fa32a6e301b6994e783be864';
+// const BASE_URL = 'https://api.themoviedb.org';
+
+const getMovies = query => {};
+
+export default getMovies;
