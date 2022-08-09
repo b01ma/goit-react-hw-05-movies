32a2d8c3b0d574a79214f0899937d5b0aa7dab29
ed_bolma <ed.bolma21@gmail.com>
2022-08-09 22:04:47 +0200

    created lazy import

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,35 @@
 import { Route, Routes } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 import css from './App.module.css';
 
-import Home from 'pages/Home';
-import Movies from 'pages/Movies';
 import Navbar from './Navbar';
 import PageNotFound from 'pages/PageNotFound';
 import Cast from './Cast';
 import Reviews from './Reviews';
-import MovieDetails from '../pages/MovieDetails';
 import Searchbar from './Searchbar/Searchbar';
+
+const Home = lazy(() => import('../pages/Home'));
+const Movies = lazy(() => import('../pages/Movies'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails'));
 
 export const App = () => {
   return (
     <div className={css.container}>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="movies" element={<Movies />}>
-          <Route index element={<Searchbar />} />
-        </Route>
-        <Route path="movies/:movieId" element={<MovieDetails />}>
-          <Route path="cast" element={<Cast />} />
-          <Route path="review" element={<Reviews />} />
-        </Route>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="movies" element={<Movies />}>
+            <Route index element={<Searchbar />} />
+          </Route>
+          <Route path="movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="review" element={<Reviews />} />
+          </Route>
 
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
